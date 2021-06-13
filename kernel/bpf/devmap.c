1203dc22dba7303e95fc2282c415e68187f78b63
Bui Quang Minh <minhquangbui99@gmail.com>
2021-06-13 21:34:39 +0700

    UPSTREAM: bpf: Fix integer overflow in argument calculation for bpf_map_area_alloc
    commit 7dd5d437c258bbf4cc15b35229e5208b87b8b4e0 upstream.
    
    In 32-bit architecture, the result of sizeof() is a 32-bit integer so
    the expression becomes the multiplication between 2 32-bit integer which
    can potentially leads to integer overflow. As a result,
    bpf_map_area_alloc() allocates less memory than needed.
    
    Fix this by casting 1 operand to u64.
    
    Fixes: 0d2c4f964050 ("bpf: Eliminate rlimit-based memory accounting for sockmap and sockhash maps")
    Fixes: 99c51064fb06 ("devmap: Use bpf_map_area_alloc() for allocating hash buckets")
    Fixes: 546ac1ffb70d ("bpf: add devmap, a map for storing net device references")
    Change-Id: I2b30a9d20d66c67f312e6c24743bcb6e208ef622
    Signed-off-by: Bui Quang Minh <minhquangbui99@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20210613143440.71975-1-minhquangbui99@gmail.com
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>

--- kernel/bpf/devmap.c
+++ kernel/bpf/devmap.c
@@ -80,12 +80,13 @@ struct bpf_dtab {
 static DEFINE_SPINLOCK(dev_map_lock);
 static LIST_HEAD(dev_map_list);
 
-static struct hlist_head *dev_map_create_hash(unsigned int entries)
+static struct hlist_head *dev_map_create_hash(unsigned int entries,
+					      int numa_node)
 {
 	int i;
 	struct hlist_head *hash;
 
-	hash = kmalloc_array(entries, sizeof(*hash), GFP_KERNEL);
+	hash = bpf_map_area_alloc((u64) entries * sizeof(*hash), numa_node);
 	if (hash != NULL)
 		for (i = 0; i < entries; i++)
 			INIT_HLIST_HEAD(&hash[i]);
@@ -162,11 +163,18 @@ static struct bpf_map *dev_map_alloc(union bpf_attr *attr)
 		goto free_dtab;
 
 	if (attr->map_type == BPF_MAP_TYPE_DEVMAP_HASH) {
-		dtab->dev_index_head = dev_map_create_hash(dtab->n_buckets);
+		dtab->dev_index_head = dev_map_create_hash(dtab->n_buckets,
+							   dtab->map.numa_node);
 		if (!dtab->dev_index_head)
 			goto free_map_area;
 
 		spin_lock_init(&dtab->index_lock);
+	} else {
+		dtab->netdev_map = bpf_map_area_alloc((u64) dtab->map.max_entries *
+						      sizeof(struct bpf_dtab_netdev *),
+						      dtab->map.numa_node);
+		if (!dtab->netdev_map)
+			goto free_dtab;
 	}
 
 	spin_lock(&dev_map_lock);
