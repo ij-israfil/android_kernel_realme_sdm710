3c032266652a6064b9b679b06c1c2fb7151e8c6d
yyq <3531236823@qq.com>
2025-01-18 13:20:38 +0800

    bpf: Add Help merge
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -2115,6 +2115,7 @@ struct tcp_ulp_ops {
 	struct list_head	list;
 	/* initialize ulp */
 	int (*init)(struct sock *sk);
+
 	/* cleanup ulp */
 	void (*release)(struct sock *sk);
 	char		name[TCP_ULP_NAME_MAX];
